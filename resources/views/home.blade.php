d5b5314ef3f69e22886bf28898f884e28055374c
Abdulazizsayed <Abdulazizsayed>
2020-10-13 13:17:00 +0200

    Finishing Header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,3 @@
 @extends('layouts.userlayout')
+
+@include('includes.home_picture')
---
+++ resources/views/includes/home_picture.blade.php
@@ -0,0 +1,13 @@
+<div class="home_picture">
+    <div class="container-fluid">
+        <div class="background-text text-center">
+            <p>
+                Start learning for <span class="free"> <strong> Free </strong></span>
+            </p>
+            <p>
+                Join {{ \App\User::all()->count() }} Users in our website!
+            </p>
+            <a class="btn btn-success" href="/register">Start learning</a>
+        </div>
+    </div>
+</div>
